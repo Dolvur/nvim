b4763135de644cc9b07bd3903e0e56bc884280b7
Daniel Karlsson <daniel.a.j.karlsson@gmail.com>
2025-06-21 10:06:42 +0200

    small lsp keymaps

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -2,22 +2,24 @@ return {
   {
     'ibhagwan/fzf-lua',
     -- lazy = false,
-    event = "VeryLazy",
+    event = 'VeryLazy',
     dependencies = { 'echasnovski/mini.icons' },
     opts = {},
     config = function(_, opts)
       local set = vim.keymap.set
+      local fzf = require 'fzf-lua'
       -- files
       set('n', '<leader>f', '<cmd>FzfLua files<CR>', { desc = 'Find Files' })
       set('n', '<leader>F', function()
-        require('fzf-lua').files { cwd = vim.fn.expand '%:p:h' }
+        fzf.files { cwd = vim.fn.expand '%:p:h' }
       end, { desc = 'Find Files' })
       -- vim.keymap.set("n", "<leader>fo", "<cmd>FzfLua files<CR>", { desc = "Find Old files" })
       -- vim.keymap.set("n", "<leader>fo", "<cmd>FzfLua files<CR>", { desc = "Find Old files" })
 
       -- search
       set('n', '<leader>sk', '<cmd>FzfLua keymaps<CR>', { desc = 'Search Keymaps' })
-      set('n', '<leader>sh', '<cmd>FzfLua helptags<CR>', { desc = 'Search Keymaps' })
+      set('n', '<leader>sh', '<cmd>FzfLua helptags<CR>', { desc = 'Search Helptags' })
+      set('n', '<leader>sd', '<cmd>FzfLua diagnostics_document<CR>', { desc = 'Search Diagnostics' })
 
       set('n', '<leader>:', '<cmd>FzfLua command_history<CR>', { desc = 'Command History' })
       set('n', '<leader>/', '<cmd>FzfLua live_grep<CR>', { desc = 'Grep' })
