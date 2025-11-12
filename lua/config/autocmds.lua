6dca1484f890408fd27f313377ceae50bbe5b21f
Daniel Karlsson <daniel.a.j.karlsson@gmail.com>
2025-11-12 15:53:22 +0100

    more autocmds

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -60,3 +60,30 @@ vim.api.nvim_create_autocmd('FileType', {
     })
   end,
 })
+
+
+-- Resize splits when the vim window is resized
+vim.api.nvim_create_autocmd('VimResized', {
+  desc = 'Resize splits when the vim window is resized',
+  group = vim.api.nvim_create_augroup('resize-splits', { clear = true }),
+  callback = function()
+    vim.cmd('tabdo wincmd =')
+  end,
+})
+
+-- DirDiff command to compare two directories
+vim.api.nvim_create_user_command('DirDiff', function(opts)
+  if vim.tbl_count(opts.fargs) ~= 2 then
+    vim.notify('DirDiff requires exactly two directory arguments', vim.log.levels.ERROR)
+    return
+  end
+
+  vim.cmd 'tabnew'
+  vim.cmd.packadd 'nvim.difftool'
+  require('difftool').open(opts.fargs[1], opts.fargs[2], {
+    rename = {
+      detect = false,
+    },
+    ignore = { '.git' },
+  })
+end, { complete = 'dir', nargs = '*' })
