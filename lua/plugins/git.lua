924a48d36829707452bcbcbde914cd59f2d4b12c
Daniel Karlsson <daniel.a.j.karlsson@gmail.com>
2026-03-18 10:05:05 +0100

    add gitsigns

---
+++ lua/plugins/git.lua
@@ -0,0 +1,16 @@
+vim.pack.add({
+  'https://github.com/lewis6991/gitsigns.nvim'
+})
+
+require('gitsigns').setup({
+  signs = {
+    add = { text = '+' },
+    change = { text = '~' },
+    delete = { text = '_' },
+    topdelete = { text = '‾' },
+    changedelete = { text = '~' },
+  }
+})
+
+vim.keymap.set('n', '<leader>gd', function() vim.cmd('Gitsigns diffthis') end, {desc = 'Gitsigns: Diffview'})
+vim.keymap.set('n', '<leader>gb', function() vim.cmd('Gitsigns blame_line') end, {desc = 'Gitsigns: Blame line'})
