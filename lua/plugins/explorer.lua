5c2cfb51088a36b796d8977e6b4e73ac191e1f6c
Daniel Karlsson <daniel.a.j.karlsson@gmail.com>
2026-03-19 14:32:07 +0100

    make yazi.nvim replace netrw for directories

--- lua/plugins/explorer.lua
+++ lua/plugins/explorer.lua
@@ -3,5 +3,11 @@ vim.pack.add {
   'https://github.com/mikavilpas/yazi.nvim',
 }
 
+require('yazi').setup {
+  open_for_directories = true,
+}
+
+vim.g.loaded_netrwPlugin = 1
+
 vim.keymap.set('n', '<leader>e', '<cmd>Yazi<CR>', { desc = 'File Explorer' })
 vim.keymap.set('n', '<leader>E', '<cmd>Yazi cwd<CR>', { desc = 'File Explorer (cwd)' })
