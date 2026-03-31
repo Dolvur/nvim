544ee5042ee887da3514d16263341e17beb4d425
Daniel Karlsson <daniel.a.j.karlsson@gmail.com>
2026-03-31 16:12:12 +0200

    disable netrw override for DiffTool

--- lua/plugins/explorer.lua
+++ lua/plugins/explorer.lua
@@ -4,10 +4,10 @@ vim.pack.add {
 }
 
 require('yazi').setup {
-  open_for_directories = true,
+  -- open_for_directories = true,
 }
 
-vim.g.loaded_netrwPlugin = 1
+-- vim.g.loaded_netrwPlugin = 1
 
 vim.keymap.set('n', '<leader>e', '<cmd>Yazi<CR>', { desc = 'File Explorer' })
 vim.keymap.set('n', '<leader>E', '<cmd>Yazi cwd<CR>', { desc = 'File Explorer (cwd)' })
